3977edac83fb6fd04fe8a50fc8f9e18ad78787b4
ilya.grozov <ilya.grozov@rozum.com>
2018-08-23 12:12:29 +0300

    add functionality

--- rozum/rest/robot.py
+++ rozum/rest/robot.py
@@ -41,18 +41,6 @@ class Robot(object):
     def identifier(self):
         return self.api.identifier()
 
-    def set_position(self, position, speed):
-        return self.api.set_position(position, speed=speed)
-
-    def run_positions(self, positions, speed):
-        return self.api.run_positions(positions, speed=speed)
-
-    def set_pose(self, pose, speed):
-        return self.api.set_pose(pose, speed=speed)
-
-    def run_poses(self, poses, speed):
-        return self.api.run_poses(poses, speed=speed)
-
     def open_gripper(self, timeout=None):
         if timeout is not None:
             return self.api.open_gripper(timeout=timeout)
@@ -61,15 +49,30 @@ class Robot(object):
     def relax(self):
         return self.api.relax()
 
-    def status_motion(self):
-        return self.api.status_motion()
+    def run_poses(self, poses, speed):
+        return self.api.run_poses(poses, speed=speed)
 
-    def await_motion(self, asking_interval=0.1):
-        while self.status_motion() != MotionStatus.IDLE:
-            time.sleep(asking_interval)
+    def run_positions(self, positions, speed):
+        return self.api.run_positions(positions, speed=speed)
+
+    def set_digital_output_high(self, port):
+        return self.api.set_digital_output_high(port)
+
+    def set_digital_output_low(self, port):
+        return self.api.set_digital_output_low(port)
+
+    def set_pose(self, pose, speed):
+        return self.api.set_pose(pose, speed=speed)
+
+    def set_position(self, position, speed):
+        return self.api.set_position(position, speed=speed)
 
     def status_motion(self):
         return self.api.status_motion()
 
     def status_motors(self):
         return self.api.status_motors()
+
+    def await_motion(self, asking_interval=0.1):
+        while self.status_motion() != MotionStatus.IDLE:
+            time.sleep(asking_interval)
