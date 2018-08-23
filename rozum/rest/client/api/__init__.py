0bca5e11180bdf7e19913311ba99285d771d7ebd
ilya.grozov <ilya.grozov@rozum.com>
2018-08-23 11:08:27 +0300

    update robot api

--- rozum/rest/client/api/__init__.py
+++ rozum/rest/client/api/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from rozum.rest.client.api.default_api import DefaultApi
+from rozum.rest.client.api.robot_api import RobotApi
