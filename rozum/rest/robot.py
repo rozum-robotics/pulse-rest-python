c505b1479693ca869a67f940166fe3f4195e3278
ilya.grozov <ilya.grozov@rozum.com>
2018-08-23 13:41:24 +0300

    api protection

--- rozum/rest/robot.py
+++ rozum/rest/robot.py
@@ -5,73 +5,73 @@ import time
 
 class Robot(object):
     def __init__(self, host=None):
-        self.api = RobotApi()
+        self._api = RobotApi()
         if host is not None:
-            self.api.api_client.configuration.host = host
+            self._api.api_client.configuration.host = host
 
     def change_base(self, position):
-        return self.api.change_base(position)
+        return self._api.change_base(position)
 
     def change_tool(self, tool):
-        return self.api.change_tool(tool)
+        return self._api.change_tool(tool)
 
     def close_gripper(self, timeout=None):
         if timeout is not None:
-            return self.api.close_gripper(timeout=timeout)
-        return self.api.close_gripper()
+            return self._api.close_gripper(timeout=timeout)
+        return self._api.close_gripper()
 
     def freeze(self):
-        return self.api.freeze()
+        return self._api.freeze()
 
     def get_base(self):
-        return self.api.get_base()
+        return self._api.get_base()
 
     def get_digital_output(self, port):
-        return self.api.get_digital_output(port)
+        return self._api.get_digital_output(port)
 
     def get_pose(self):
-        return self.api.get_pose()
+        return self._api.get_pose()
 
     def get_position(self):
-        return self.api.get_position()
+        return self._api.get_position()
 
     def get_tool(self):
-        return self.api.get_tool()
+        return self._api.get_tool()
 
     def identifier(self):
-        return self.api.identifier()
+        return self._api.identifier()
 
     def open_gripper(self, timeout=None):
         if timeout is not None:
-            return self.api.open_gripper(timeout=timeout)
-        return self.api.open_gripper()
+            return self._api.open_gripper(timeout=timeout)
+        return self._api.open_gripper()
 
     def relax(self):
-        return self.api.relax()
+        return self._api.relax()
 
     def run_poses(self, poses, speed):
-        return self.api.run_poses(poses, speed=speed)
+        return self._api.run_poses(poses, speed=speed)
 
     def run_positions(self, positions, speed):
-        return self.api.run_positions(positions, speed=speed)
+        return self._api.run_positions(positions, speed=speed)
 
     def set_digital_output_high(self, port):
-        return self.api.set_digital_output_high(port)
+        return self._api.set_digital_output_high(port)
 
     def set_digital_output_low(self, port):
-        return self.api.set_digital_output_low(port)
+        return self._api.set_digital_output_low(port)
 
     def set_pose(self, pose, speed):
-        return self.api.set_pose(pose, speed=speed)
+        return self._api.set_pose(pose, speed=speed)
 
     def set_position(self, position, speed):
-        return self.api.set_position(position, speed=speed)
+        return self._api.set_position(position, speed=speed)
 
     def status_motion(self):
-        return self.api.status_motion()
+        return self._api.status_motion()
 
     def status_motors(self):
-        return self.api.status_motors()
+        return self._api.status_motors()
 
     def await_motion(self, asking_interval=0.1):
         while self.status_motion() != MotionStatus.IDLE:
