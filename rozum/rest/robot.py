80ee6aa2b5db7ab861713a1031ab72d00fddee58
ilya.grozov <ilya.grozov@rozum.com>
2018-08-23 11:37:49 +0300

    change base and tool

--- rozum/rest/robot.py
+++ rozum/rest/robot.py
@@ -9,6 +9,17 @@ class Robot(object):
         if host is not None:
             self.api.api_client.configuration.host = host
 
+    def change_base(self, position):
+        return self.api.change_base(position)
+
+    def change_tool(self, tool):
+        return self.api.change_tool(tool)
+
+    def close_gripper(self, timeout=None):
+        if timeout is not None:
+            return self.api.close_gripper(timeout=timeout)
+        return self.api.close_gripper()
+
     def get_position(self):
         return self.api.get_position()
 
@@ -31,11 +42,6 @@ class Robot(object):
         if timeout is not None:
             return self.api.open_gripper(timeout=timeout)
         return self.api.open_gripper()
-
-    def close_gripper(self, timeout=None):
-        if timeout is not None:
-            return self.api.close_gripper(timeout=timeout)
-        return self.api.close_gripper()
 
     def relax(self):
         return self.api.relax()
